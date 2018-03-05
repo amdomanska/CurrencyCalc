e259fd96329857bdc63eea7ee6121e85a0d017b8
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-03-05 22:13:28 +0000

    url parsing

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
+"use strict"
+
 var http = require("http");
+var url = require("url");
+var querystring = require("querystring");
 
-http.createServer(function (request, response) {
+http.createServer(function (req, res) {
 
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
+   res.writeHead(200, {'Content-Type': 'text/html'});
+   let page = url.parse(req.url).pathname;
+   let params = querystring.parse(url.parse(req.url).query);
+
+   console.log(page);
+   res.write("Welcome ");
+   if ('firstname' in params && 'lastname' in params) {
+     res.write(params['firstname'] + ' ' + params['lastname']);
+   }
+   res.write(" in my ");
+   if (page == '/') {
+     res.write("main room!");
+   }
+   else if (page == '/bathroom'){
+     res.write("bathroom!");
+   }
+
+   res.end();
 }).listen(8081);
 
 // Console will print the message
