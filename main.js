e7c589b95b92026bd23c50ce9658d1c7b746f411
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-03-08 21:20:10 +0000

    ejs modul installed; basic web generated; get and post method;

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 "use strict"
 
-let express = require('express');
-let bodyParser = require('body-parser');
-let app = express();
+const fetch = require('node-fetch');
+const express = require('express');
+const bodyParser = require('body-parser');
+const url = require('url');
+
+const app = express();
+
+let passedParams = {value: 0,currA: 0, currB: 0, rate: 0, result: 0};
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -12,27 +17,29 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })
 
 app.get('/main', function(req, res) {
-  res.render('main.ejs');
+  res.render('mainView.ejs',{value: passedParams.value,
+                            currA: passedParams.currA,
+                            currB: passedParams.currB,
+                            rate: passedParams.rate,
+                            result: passedParams.result});
 })
 
-.post('/main', function(req, res) {
-  if (req.body.currA != '' && req.body.currB != '') {
-    let currA = req.body.currA;
-    let currB = req.body.currB;
+.post('/main/calc', function(req, res) {
 
-  fetch('https://api.fixer.io/latest?symbols='+'currA'+'currB')
+  if (req.body.currA != '' && req.body.currB != '') {
+
+    passedParams= {value: req.body.value, currA: req.body.currA, currB: req.body.currB, rate: 1, result: 1};
+
+  /*fetch('https://api.fixer.io/latest?symbols='+'currA'+'currB')
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson);
-  });
+  });*/
 
-  }
-  res.redirect('/main',{currA: req.body.currA,
-                        currB: req.body.currB,
-                        rate: 0,
-                        result: 0});
+}
+  res.redirect('/main');
 })
 
 
