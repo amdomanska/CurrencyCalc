dccabd5b0c2c30b34ccd4520639268b6a8b631ab
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-03-11 22:22:11 +0000

    dropdown select with available currencies added; bootstrap installed for styles;

--- main.js
+++ main.js
@@ -7,46 +7,70 @@ const url = require('url');
 
 const app = express();
 
-let passedParams = {value: 0,currA: 0, currB: 0, rate: 0, result: 0};
+let passedParams = {value: 0, currA: 0, currB: 0, rate: 0, result: 0};
 
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
 .use(function(req, res, next){
-    next();
+  next();
 })
+.use(express.static('public'))
 
-app.get('/main', function(req, res) {
-  res.render('mainView.ejs',{value: passedParams.value,
-                            currA: passedParams.currA,
-                            currB: passedParams.currB,
-                            rate: passedParams.rate,
-                            result: passedParams.result});
+.get('/main', function(req, res) {
+
+  fetch('https://api.fixer.io/latest')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    let currList = Object.keys(myJson.rates);
+    res.render('mainView.ejs', {currList: currList,
+                                value: passedParams.value,
+                                currA: passedParams.currA,
+                                currB: passedParams.currB,
+                                rate: passedParams.rate,
+                                result: passedParams.result});
+    //console.log(currList);
+  });
+
 })
 
 .post('/main/calc', function(req, res) {
 
-  if (req.body.currA != '' && req.body.currB != '') {
+  if (req.body.currA != 0 && req.body.currB != 0) {
+
     fetch('https://api.fixer.io/latest?symbols='+req.body.currA+','+req.body.currB)
     .then(function(response) {
       return response.json();
     })
     .then(function(myJson) {
       console.log(myJson);
+
       let rateCurrA = myJson.rates[req.body.currA];
       let rateCurrB = myJson.rates[req.body.currB];
       console.log(rateCurrA,rateCurrB);
+
       let rate = Math.round((rateCurrA/rateCurrB) * 10000) / 10000;
       let result = Math.round((req.body.value * (rateCurrB/rateCurrA)) * 100) / 100;
-      console.log(rate,result);
+      //console.log(rate,result);
 
-      passedParams= {value: req.body.value, currA: req.body.currA,
-                    currB: req.body.currB,
-                    rate: rate,
-                    result: result};
+      passedParams = {value: req.body.value,
+                      currA: req.body.currA,
+                      currB: req.body.currB,
+                      rate: rate,
+                      result: result};
       res.redirect('/main');
     });
-}
+  }
+  else {
+    passedParams = {value: 0,
+                    currA: 0,
+                    currB: 0,
+                    rate: 0,
+                    result: 0};
+    res.redirect('/main');
+  }
 })
 
 
