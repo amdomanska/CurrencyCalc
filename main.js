7aed43beb25529534e562ba76092f9ea46df1e6d
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-03-14 20:36:21 +0000

    global params replaced with express-session params; eslint configured; main style improved;

--- main.js
+++ main.js
@@ -1,81 +1,71 @@
-"use strict"
+'use strict'
 
-const fetch = require('node-fetch');
-const express = require('express');
-const bodyParser = require('body-parser');
-const url = require('url');
+const fetch = require('node-fetch')
+const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 
-const app = express();
+const app = express()
 
-let passedParams = {value: 0, currA: 0, currB: 0, rate: 0, result: 0};
+// let passedParams = {value: 0, currA: 0, currB: 0, rate: 0, result: 0};
 
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }))
-.use(function(req, res, next){
+/* .use(function(req, res, next){
   next();
-})
-.use(express.static('public'))
+}) */
+  .use(express.static('public'))
+  .use(session({
+    secret: 'curr calc secret',
+    resave: false,
+    saveUninitialized: true
+  }))
 
-.get('/main', function(req, res) {
+const zeroParams = () => ({value: 0, currA: 0, currB: 0, rate: 0, result: 0})
 
+app.get('/main', function (req, res) {
+  if (!req.session.passedParams) {
+    req.session.passedParams = zeroParams()
+  }
   fetch('https://api.fixer.io/latest')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    let currList = Object.keys(myJson.rates);
-    res.render('mainView.ejs', {currList: currList,
-                                value: passedParams.value,
-                                currA: passedParams.currA,
-                                currB: passedParams.currB,
-                                rate: passedParams.rate,
-                                result: passedParams.result});
-    //console.log(currList);
-  });
-
-})
-
-.post('/main/calc', function(req, res) {
-
-  if (req.body.currA != 0 && req.body.currB != 0) {
-
-    fetch('https://api.fixer.io/latest?symbols='+req.body.currA+','+req.body.currB)
-    .then(function(response) {
-      return response.json();
+    .then(response => response.json())
+    .then(myJson => {
+      let currList = Object.keys(myJson.rates)
+      res.render('mainView.ejs', {
+        currList: currList,
+        ...req.session.passedParams
+      })
     })
-    .then(function(myJson) {
-      console.log(myJson);
+  })
 
-      let rateCurrA = myJson.rates[req.body.currA];
-      let rateCurrB = myJson.rates[req.body.currB];
-      console.log(rateCurrA,rateCurrB);
+  .post('/main/calc', function (req, res) {
+    if (req.body.currA != 0 && req.body.currB != 0) { // eslint-disable-line
+      fetch(`https://api.fixer.io/latest?symbols=${req.body.currA},$(req.body.currB)`)
+        .then(response => response.json())
+        .then(myJson => {
+          // console.log(myJson);
+          let rateCurrA = myJson.rates[req.body.currA]
+          let rateCurrB = myJson.rates[req.body.currB]
+          // console.log(rateCurrA,rateCurrB);
+          let rate = Math.round((rateCurrB / rateCurrA) * 10000) / 10000
+          let result = Math.round((req.body.value * (rateCurrB / rateCurrA)) * 100) / 100
+          // console.log(rate,result);
 
-      let rate = Math.round((rateCurrB/rateCurrA) * 10000) / 10000;
-      let result = Math.round((req.body.value * (rateCurrB/rateCurrA)) * 100) / 100;
-      //console.log(rate,result);
+          req.session.passedParams = {value: req.body.value,
+            currA: req.body.currA,
+            currB: req.body.currB,
+            rate: rate,
+            result: result}
+          res.redirect('/main')
+        })
+    } else {
+      req.session.passedParams = zeroParams()
+      res.redirect('/main')
+    }
+  })
 
-      passedParams = {value: req.body.value,
-                      currA: req.body.currA,
-                      currB: req.body.currB,
-                      rate: rate,
-                      result: result};
-      res.redirect('/main');
-    });
-  }
-  else {
-    passedParams = {value: 0,
-                    currA: 0,
-                    currB: 0,
-                    rate: 0,
-                    result: 0};
-    res.redirect('/main');
-  }
+app.use(function (req, res, next) {
+  res.redirect('/main')
 })
-
-
-
-.use(function(req, res, next){
-    res.redirect('/main');
-})
-.listen(8080);
+  .listen(8080)
